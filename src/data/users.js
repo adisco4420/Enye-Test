2a8541fc54fd27839ef935fbaef84a36d00eadd9
alabi sodiq <adisco4420@gmail.com>
2019-10-24 18:10:23 +0100

    user dummy data

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export const Users = [
+    {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Ken',
+      birthday: new Date(1978, 12, 2).toDateString(),
+      age: 32,
+      hobby: 'Singing',
+    },
+    {
+      id: 2,
+      firstName: 'Jim',
+      lastName: 'Green',
+      birthday: new Date(1967, 4, 5).toDateString(),
+      age: 27,
+      hobby: 'Dancing',
+    },
+    {
+      id: 3,
+      firstName: 'Joe',
+      lastName: 'Black',
+      birthday: new Date(1990, 21, 12).toDateString(),
+      age: 43,
+      hobby: 'Coding',
+    },
+  ];
