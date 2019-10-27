d63ac5f9d1b4321425acd8bc01d392b068e8332a
alabi sodiq <adisco4420@gmail.com>
2019-10-27 23:19:40 +0100

    listen to firebase

---
+++ src/users/List/Sagas.js
@@ -0,0 +1,35 @@
+import { put, fork, take } from "redux-saga/effects";
+import { eventChannel } from "redux-saga";
+import firebase from "firebase";
+import { ActionCreators } from "./Actions";
+import firebaseConfig from '../../firebase-config';
+
+firebase.initializeApp(firebaseConfig);
+
+function* startListener() {
+  // #1: Creates an eventChannel and starts the listener;
+  const channel = new eventChannel(emiter => {
+    const listener = firebase
+      .database()
+      .ref("test-user")
+      .on("value", snapshot => {
+        emiter({ data: snapshot.val() || {} });
+      });
+
+    // #2: Return the shutdown method;
+    return () => {
+      listener.off();
+    };
+  });
+
+  // #3: Creates a loops to keep the execution in memory;
+  while (true) {
+    const { data } = yield take(channel);
+    // #4: Pause the task until the channel emits a signal and dispatch an action in the store;
+    yield put(ActionCreators.updateList(data));
+  }
+}
+
+export default function* root() {
+  yield fork(startListener);
+}
