ab0689288828f634ee336defa0f6615a4a6346d8
alabi sodiq <adisco4420@gmail.com>
2019-10-27 23:18:28 +0100

    firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDTMzO1i7nSMoA9YjolBPzhABnJ54VqaNc",
+    authDomain: "oshop-591ef.firebaseapp.com",
+    databaseURL: "https://oshop-591ef.firebaseio.com",
+    projectId: "oshop-591ef",
+    storageBucket: "oshop-591ef.appspot.com",
+    messagingSenderId: "1032986405877",
+    appId: "1:1032986405877:web:e8486ad1802a5dd7a14ebc"
+  };
+  
+  export default firebaseConfig;
